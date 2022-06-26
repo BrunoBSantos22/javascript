f6d279fe030f389ed7055507bf04121621b04223
Bruno <brunosantos3225@gmail.com>
2022-06-25 22:24:09 -0300

    Conditions in JavaScript.
    I learned to use a simple condition, and a compound condition in JavaScript. Just missing to style the content.

---
+++ exercicios-curso-em-video/aula11/ex001.js
@@ -0,0 +1,9 @@
+var vel = 66
+
+console.log(`O carro está a ${vel}km/hr`)
+
+if (vel > 65) { // Condição Simples
+	console.log('Você acaba de receber uma MULTA por excesso de velocidade.')
+}
+
+console.log('Dirija sempre com responsabilidade!')
---
+++ exercicios-curso-em-video/aula11/ex002.js
@@ -0,0 +1,9 @@
+var país = 'Brasil'
+
+console.log(`Se estiver em ${país}`)
+
+if (país == 'Brasil') {
+	console.log('Você é Brasileiro!')
+} else {
+	console.log('Você é Estrangeiro!')
+}
