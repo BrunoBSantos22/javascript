df40071bb52a2ce09ebddc456b32007aec9f5a61
Bruno <brunosantos3225@gmail.com>
2022-07-14 21:35:41 -0300

    Writing the challenge's JavaScript.
    I started the JavaScript part by doing some small tests.

--- desafios-curso-em-video/desafios-condicoes/d002/js/script.js
+++ desafios-curso-em-video/desafios-condicoes/d002/js/script.js
@@ -1,3 +1,14 @@
 function verificar() {
-	
+	var data = new Date()
+	var ano = data.getFullYear()
+	var fano = document.getElementById('txtano')
+	var res = document.getElementById('res')
+
+	if (fano.value == 0 || fano.value > ano) {
+		alert('[ERRO] Verifique os dados informados, e tente novamente.')
+	} else {
+		var fsex = document.getElementsByName('radsex')
+		var idade = ano - Number(fano.value)
+		res.innerHTML = `Você tem ${idade} anos.`
+	}
 }
