a3ebd7ba94a9ccd2b3e2aa72f2e33e27e0856fbb
Bruno <brunosantos3225@gmail.com>
2022-07-15 21:28:48 -0300

    Challenge concluded.
    Challenge completed. But, I need to edit the photos better, just this detail that I will fix to be perfect.

--- desafios-curso-em-video/desafios-condicoes/d002/js/script.js
+++ desafios-curso-em-video/desafios-condicoes/d002/js/script.js
@@ -9,6 +9,43 @@ function verificar() {
 	} else {
 		var fsex = document.getElementsByName('radsex')
 		var idade = ano - Number(fano.value)
-		res.innerHTML = `Você tem ${idade} anos.`
+		var genero = ''
+		var img = document.createElement('img')
+		img.setAttribute('id', 'foto')
+
+		if (fsex[0].checked) {
+			genero = 'Homem'
+			if (idade >= 0 && idade < 15) {
+				//criança
+				img.setAttribute('src', 'fotos/crianca-mas.png')
+			} else if (idade < 25) {
+				//jovem
+				img.setAttribute('src', 'fotos/jovem-mas.png')
+			} else if (idade < 55) {
+				//adulto
+				img.setAttribute('src', 'fotos/adulto-mas.png')
+			} else {
+				//idoso
+				img.setAttribute('src', 'fotos/idoso-mas.png')
+			}
+		} else if (fsex[1].checked) {
+			genero = 'Mulher'
+			if (idade >= 0 && idade < 15) {
+				//criança
+				img.setAttribute('src', 'fotos/crianca-fem.png')
+			} else if (idade < 25) {
+				//jovem
+				img.setAttribute('src', 'fotos/jovem-fem.png')
+			} else if (idade < 55) {
+				//adulto
+				img.setAttribute('src', 'fotos/adulto-fem.png')
+			} else {
+				//idoso
+				img.setAttribute('src', 'fotos/idoso-fem.png')
+			}
+		} 
+		res.style.textAlign = 'center'
+		res.innerHTML = `Sou ${genero}, e tenho ${idade} anos.`
+		res.appendChild(img)
 	}
 }
