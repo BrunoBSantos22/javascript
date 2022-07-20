1e803e04d84052f75230711d742c096afe316773
Bruno <brunosantos3225@gmail.com>
2022-07-20 14:53:43 -0300

    Ending the JavaScript Repetitions exercise.
    Learning to use two forms of repetitions, to be used in different situations.

--- exercicios-curso-em-video/aula13/ambiente.js
+++ exercicios-curso-em-video/aula13/ambiente.js
@@ -1,10 +1,19 @@
 var c = 1
-while (c <= 10) {
-	console.log('Oi? Como se tá?')
-	
-}
+do {
+	console.log(`Cheguei em ${c}° lugar!`)
+	c++ /* c = c + 1 */
+} while (c <= 10)		  /*  ---TESTE LÓGICO NO FINAL */
 
 
+
+
+
+
+/*var c = 1
+while (c <= 10) {							---TESTE LÓGICO NO INÍCIO
+	console.log(`Cheguei em ${c}° lugar!`)	
+	c++   	 / c = c + 1 /
+} */
 
 
 
